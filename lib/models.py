c65bb1bac05b7d86801d766d9d90730d862d9952
Bisinle <Abdiwadud.mohamedd@gmail.com>
2023-08-31 17:43:26 +0300

    one to many, passed all the tests

--- lib/models.py
+++ lib/models.py
@@ -8,3 +8,52 @@ convention = {
 metadata = MetaData(naming_convention=convention)
 
 Base = declarative_base(metadata=metadata)
+
+# example
+
+class Customer(Base):
+    __tablename__ = 'customers'
+    id = Column(Integer(), primary_key=True)
+    name = Column(String())
+    orders = relationship('Order', backref='customer')
+  
+
+class Order(Base):
+    __tablename__ = 'orders'
+    id = Column(Integer(), primary_key=True)
+    title = Column(String())
+    customer_id = Column(Integer(), ForeignKey('customers.id'))
+
+
+
+#----------------------------------------------------------------------
+class Game(Base):
+    __tablename__ = 'games'
+
+    id = Column(Integer(), primary_key=True)
+    title = Column(String())
+    genre = Column(String())
+    platform = Column(String())
+    price = Column(Integer())
+    reviews = relationship('Review', backref=backref('game'), cascade='all, delete-orphan')
+    
+    
+    def __repr__(self):
+        return f'Game(id={self.id}, ' + \
+            f'title={self.title}, ' + \
+            f'platform={self.platform})'
+ 
+
+class Review(Base):
+    __tablename__ = 'reviews'
+
+    id = Column(Integer(), primary_key=True)
+    score = Column(Integer())
+    comment = Column(String())
+    game_id = Column(Integer(), ForeignKey('games.id'))
+
+
+    def __repr__(self):
+        return f'Review(id={self.id}, ' + \
+            f'score={self.score}, ' + \
+            f'game_id={self.game_id})'
--- lib/seed.py
+++ lib/seed.py
@@ -6,15 +6,58 @@ import random
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-from models import Game, Review
+from models import  *
 
 if __name__ == '__main__':
     engine = create_engine('sqlite:///one_to_many.db')
     Session = sessionmaker(bind=engine)
     session = Session()
 
+    '''
+
+    # session.query(Customer).delete()
+    # # make user you correctly name the attr you are initializing the object with 
+    # hubert = Customer(name ='kiniop')
+    # zack = Customer(name='zack')
+    # charlez = Customer(name='charlez')
+    # hether = Customer(name='hether')
+    
+    # session.add_all([hubert, zack,charlez,hether])
+    # session.commit()
+    # res = (session.query(Customer).all())
+    # #print the orders that belongs to a customer
+    # print(charlez.orders)
+    # # if a customer has more than on order, python gives an array of the oder objet
+    # # we can access then using the index
+    # print('for zack:-> ',zack.orders[0].title)
+    # print('for zack:-> ',zack.orders[1].title)
+
+    # # for i in res:
+    # #     print(f"{i.id} {i.name}")
+
+
+
+# ----------- populate the order table
+    session.query(Order).delete()
+    # make user you correctly name the attr you are initializing the object with 
+    order1 = Order(title='shwarma', customer=zack)
+    order2 = Order(title='milk shake', customer=charlez)
+    order3 = Order(title='oil', customer=hether)
+    order4 = Order(title='nyamachoma', customer=zack)
+
+    session.add_all([order1,order2,order3,order4])
+    session.commit()
+    # ----------- get the rcords from the order table
+    orders = session.query(Order).filter(Order.id == 2)
+    # we can reverse access the customer, from the order, making use of the 
+    #backref variable provided = customer
+    print(order1.customer.id,order1.customer.name)
+   
+    # for order in orders:
+    #     print(order.id, order.customer.name)'''
+
+
     session.query(Game).delete()
-    session.query(Review).delete()
 
     fake = Faker()
 
@@ -23,6 +66,7 @@ if __name__ == '__main__':
     platforms = ['nintendo 64', 'gamecube', 'wii', 'wii u', 'switch',
         'playstation', 'playstation 2', 'playstation 3', 'playstation 4',
         'playstation 5', 'xbox', 'xbox 360', 'xbox one', 'pc']
+  
 
     games = []
     for i in range(50):
@@ -38,8 +82,12 @@ if __name__ == '__main__':
         session.commit()
 
         games.append(game)
+        # session.add_all(games)
+        # session.commit()
+        # print(game)
 
-    reviews = []
+    session.query(Review).delete()
+    reviews_list = []
     for game in games:
         for i in range(random.randint(1,5)):
             review = Review(
@@ -48,8 +96,52 @@ if __name__ == '__main__':
                 game_id=game.id
             )
 
-            reviews.append(review)
+            reviews_list.append(review)
     
-    session.bulk_save_objects(reviews)
+    session.bulk_save_objects(reviews_list)
     session.commit()
     session.close()
+
+    '''----------------if we manually wanna delete the reviews data
+      all_review = session.query(Review)
+    for rev in all_review:
+        session.delete(rev)
+    session.commit()
+
+    '''
+ 
+    # reviews = session.query(Review).filter(Review.score <1).first()
+    # for rev in reviews:
+    #     print(rev.game.title)
+    
+    # print(reviews.game) # should give you the game this rev belongs to 
+
+    # ------------------------------------
+# #     #lets access the game and the one to many relshp it has with reviews
+    g = session.query(Game).first()
+    print(g)
+    print('-----------------')
+
+    game1_reviews = ([  rev for rev in session.query(Review).filter_by(game_id = g.id)])
+    print((game1_reviews))
+    print('-----------------')
+ # lets see how many    reviews this game has
+    # print(g.reviews)
+
+    # lets access the game with ID 2 and delete it 
+    # and then we see if all the review associated with it are gone as well
+    # game_with_id_2 = session.query(Game).filter(Game.id == 2)
+    # id =0
+    
+    # for i in game_with_id_2:
+    #     id = i.id
+    #     game = i
+    # print(id)
+    # session.delete(game)
+    # # lets get all the reviews for the game_with_id_2
+    # game_with_id_2_reviews= session.query(Review).filter_by(game_id = id).all()
+    # print(game_with_id_2_reviews)
+ 
+
+
+   
